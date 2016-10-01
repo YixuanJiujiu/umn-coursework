366f83d0335b7e69fa60936c1c41fa298075957e
GnUfTw <maxpung@gmail.com>
2016-09-30 23:15:20 -0500

    Completed previous, next, & bullet controls. Implemented updating of shown image tooltips. And fixed logical error in use of repeated if statements (causing next to always show the first ad).

--- CSCI-4131/Assignment 4/advertisement.js
+++ CSCI-4131/Assignment 4/advertisement.js
@@ -1,17 +1,14 @@
-firstAd = new Image();
-secondAd = new Image();
-thirdAd = new Image();
-activeBullet = new Image();
-nonactiveBullet = new Image();
-hoverBullet = new Image();
-next = new Image();
-prev = new Image();
-hoverNext = new Image();
-hoverPrev = new Image();
-
+// Advertisement images.
 firstAd = "advertisement-images/Nerve.jpg";
 secondAd = "advertisement-images/secret-life-of-pets.jpg";
 thirdAd = "advertisement-images/suicide-squad.jpg";
+
+// Advertisement image tooltips.
+firstAdTooltip = "Nerve Fri, Sep 30 7:00PM";
+secondAdTooltip = "secret-life-of-pets Fri, Oct 7 7:00PM";
+thirdAdTooltip = "suicide-squade Fri, Oct 14 8:00PM";
+
+// Control images.
 activeBullet = "advertisement-images/bullet_blue.png";
 nonactiveBullet = "advertisement-images/bullet_gray.png";
 hoverBullet = "advertisement-images/bullet_orange.png";
@@ -19,68 +16,135 @@ next = "advertisement-images/next_blue.png";
 prev = "advertisement-images/prev_blue.png";
 hoverNext = "advertisement-images/next_orange.png";
 hoverPrev = "advertisement-images/prev_orange.png";
-var currentBulletId;
-var count;
 
-//var advertisements = [];
-//advertisements[0] = firstAd;
-//advertisements[1] = secondAd;
-//advertisements[2] = thirdAd;
+var currentBulletId;    // Holds the element id of the currently selected (active) bullet.
+var count;              // Seconds that have elapsed since the page was loaded.
+var startTime;          // Used to calculate intervals to show different advertisements.
 
 // Register event handlers for overriding buttons.
 function start()
 {
-    // event listener for next
-    // event listener for prev
-    // event listener for bullet1
-    // event listener for bullet2
-    // event listener for bullet3
+    document.getElementById("nextImage").addEventListener("click", goToNext);
+    document.getElementById("prevImage").addEventListener("click", goToPrev);
+    document.getElementById("bullet1").addEventListener("click", changeToFirstAd);
+    document.getElementById("bullet2").addEventListener("click", changeToSecondAd);
+    document.getElementById("bullet3").addEventListener("click", changeToThirdAd);
 }
 
 function startTimer()
 {
     count = 0;
-    currentBulletId = "bullet1";
-    setInterval(cycleImages, 1000);
-    return;
+    startTime = 0;
+    currentBulletId = "bullet1";    // start with the nerve advertisement
+    var doReset = setInterval(cycleImages, 1000);   // update the slideshow every second
 }
 
+// This function decides if the current advertisement should be updated or not.
 function cycleImages()
 {
-    switch (count)
-    {
-        case 7:
-            // change ad to secret life of pets
-            document.getElementById("adImage").src = secondAd;
-            document.getElementById("bullet1").src = nonactiveBullet;
-            document.getElementById("bullet2").src = activeBullet;
-            currentBulletId = "bullet2";
-            count++;
-            break;
-        case 12:
-            // change ad to suicide squad
-            document.getElementById("adImage").src = thirdAd;
-            document.getElementById("bullet2").src = nonactiveBullet;
-            document.getElementById("bullet3").src = activeBullet;
-            currentBulletId = "bullet3";
-            count++;
-            break;
-        case 15:
-            // change ad to nerve
-            document.getElementById("adImage").src = firstAd;
-            document.getElementById("bullet3").src = nonactiveBullet;
-            document.getElementById("bullet1").src = activeBullet;
-            currentBulletId = "bullet1";
-            count = 0;
-            break;
-        default:
-            count++;
-            break;
-    }
+    var timeElapsed = count - startTime;
     
-    return;
+    if ((timeElapsed == 7) && (currentBulletId == "bullet1"))
+    {       // change ad to secret life of pets
+            changeToSecondAd();
+            count++;
+    }
+    else if ((timeElapsed == 5) && (currentBulletId == "bullet2"))
+    {
+            // change ad to suicide squad
+            changeToThirdAd();
+            count++;
+    }
+    else if ((timeElapsed == 3) && (currentBulletId == "bullet3"))
+    {
+            // change ad to nerve
+            changeToFirstAd();
+            count;
+    }
+    else
+    {
+            count++;    // no ad change necessary
+    }
 }
 
+// Event handler for when the next button is selected. It shows the next
+// ad in the slideshow, if the third ad is being shown...it loops back 
+// to the first.
+function goToNext()
+{
+    if (currentBulletId == "bullet1")
+    {
+        changeToSecondAd();
+    }
+    else if (currentBulletId == "bullet2")
+    {
+        changeToThirdAd();
+    }
+    
+    else if (currentBulletId == "bullet3")
+    {
+        changeToFirstAd();   
+    }
+}
+
+// Event handler for when the previous button is selected. It shows the previous
+// ad in the slideshow, if the first is being shown...it loops back to the third.
+function goToPrev()
+{
+    if (currentBulletId == "bullet1")
+    {
+        changeToThirdAd();
+    }
+    else if (currentBulletId == "bullet2")
+    {
+        changeToFirstAd();
+    }
+    
+    else if (currentBulletId == "bullet3")
+    {
+        changeToSecondAd();   
+    }
+}
+
+function changeToSecondAd()
+{
+    document.getElementById("adImage").src = secondAd;                                              // Update advertisement image
+    document.getElementById("adImage").title = secondAdTooltip;                                     // Update advertisement tooltip
+    document.getElementById("adLink").href = "http://sua.umn.edu/events/calendar/event/14786/";     // Update advedrtisement link
+    document.getElementById("bullet1").src = nonactiveBullet;                                       // Update bullet states accordingly
+    document.getElementById("bullet2").src = activeBullet;
+    document.getElementById("bullet3").src = nonactiveBullet;
+    currentBulletId = "bullet2";
+    startTime = count;
+}
+    
+function changeToThirdAd()
+{
+    document.getElementById("adImage").src = thirdAd;
+    document.getElementById("adImage").title = thirdAdTooltip;
+    document.getElementById("adLink").href = "http://sua.umn.edu/events/calendar/event/14794/";
+    document.getElementById("bullet1").src = nonactiveBullet;
+    document.getElementById("bullet2").src = nonactiveBullet;
+    document.getElementById("bullet3").src = activeBullet;
+    currentBulletId = "bullet3";
+    startTime = count;
+}
+    
+function changeToFirstAd()
+{
+    document.getElementById("adImage").src = firstAd;
+    document.getElementById("adImage").title = firstAdTooltip;
+    document.getElementById("adLink").href = "http://sua.umn.edu/events/calendar/event/14781/";
+    document.getElementById("bullet1").src = activeBullet;
+    document.getElementById("bullet2").src = nonactiveBullet;
+    document.getElementById("bullet3").src = nonactiveBullet;
+    currentBulletId = "bullet1";
+    startTime = count;
+}
+
+// Event handler for when control images are being hovered over.
+// When they are hovered over, the images are replaced with their
+// orange equivalents (which indicate the hover).
 function mouseOver(event)
 {
     var id = event.target.getAttribute("id");
@@ -98,6 +162,9 @@ function mouseOver(event)
     }
 }
 
+// Event handler for when leaving hover of control iamges. Upon
+// leaving a hover, the given image will be returned to it's 
+// original (non-hover) source.
 function mouseOut(event)
 {
     var id = event.target.getAttribute("id");
@@ -122,7 +189,7 @@ function mouseOut(event)
     }
 }
 
-//window.addEventListener("load", start, false);
-document.addEventListener("mouseover", mouseOver, false);
+window.addEventListener("load", start, false);              // Start button click event listeners on window load.
+document.addEventListener("mouseover", mouseOver, false);   
 document.addEventListener("mouseout", mouseOut, false);
-window.onload = startTimer;
+window.onload = startTimer;                                 // Start slideshow functionality on window load.

b64e86230819205975b8a0cb00065e1b36650a51
GnUfTw <maxpung@gmail.com>
2016-10-25 21:09:05 -0500

    Cleaned up formatting, added comments, removed unnecessary code

--- CSCI-5103/Assignment 4/src/problem2/producer.c
+++ CSCI-5103/Assignment 4/src/problem2/producer.c
@@ -7,35 +7,32 @@
 #include <stdlib.h>	// for atoi, exit
 #include <pthread.h>	// for mutex & conditon variables
 
-#define DEBUG
-#define IPC_ALLOC 0
-
 const char black_fn[] = "producer_black.txt";
 static const char white_fn[] = "producer_white.txt";
 static const char red_fn[] = "producer_red.txt";
 static const char black[] = "BLACK";
 static const char white[] = "WHITE";
 static const char red[] = "RED";
-struct timeval timestamp;
 
 #define MAX_ITEM_SIZE (sizeof(black) + (2 * sizeof(char)) + sizeof(long int)) 
+#define IPC_ALLOC 0	// for getting a shared mem segment that already exists
 
+// See comments on this structure in main.c
 typedef struct buffer
 {
         char item_buffer[2][MAX_ITEM_SIZE];
         pthread_mutex_t lock;
         pthread_cond_t space_available, item_available;
         int buf_count;
-        int finished_producers;
 } shm_buffer;
 
 int main(int argc, char* argv[])
 {
-	char *color_name = argv[1];
-	int shmid = 0;
-	shm_buffer *shmptr;
+	char *color_name = argv[1];	// parent passed us name of color give producer should produce
+	int shmid = 0;			// shared memory identifier
+	shm_buffer *shmptr;		// pointer to shared memory segment
 	
-	// attach to shared memory
+	// attach to existing shared memory
 	if ((shmid = shmget(atoi(argv[2]), 0, IPC_ALLOC)) == -1)
 	{
 		fprintf(stderr, "error in producer getting shared mem id: %s\n", strerror(errno));
@@ -47,10 +44,9 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 
-	
-
+	// determine which color to produce and produce it
 	int cmp_result = 0;
-	if ((cmp_result = strcmp(color_name, black)) == 0)
+	if ((cmp_result = strcmp(color_name, black)) == 0)	
 	{
 		FILE *black_fp;
 		if ((black_fp = fopen(black_fn, "a")) == NULL)
@@ -65,18 +61,18 @@ int main(int argc, char* argv[])
 			pthread_mutex_lock(&(shmptr->lock));
 			while (shmptr->buf_count == 2)
 				while (pthread_cond_wait(&(shmptr->space_available), &(shmptr->lock)) != 0);
+
 			char item_str[MAX_ITEM_SIZE];
 			struct timeval timestamp;
 			gettimeofday(&timestamp, NULL);
-			snprintf(item_str, MAX_ITEM_SIZE, "%s %li", black, timestamp.tv_usec);
-			strcpy(shmptr->item_buffer[shmptr->buf_count], item_str);
+			snprintf(item_str, MAX_ITEM_SIZE, "%s %li", black, timestamp.tv_usec);	// create item
+			strcpy(shmptr->item_buffer[shmptr->buf_count], item_str);		// place item in buffer
 			shmptr->buf_count++;
+
 			pthread_mutex_unlock(&(shmptr->lock));
 			pthread_cond_signal(&(shmptr->item_available));
-			fprintf(black_fp, "%s\n", item_str);
+			fprintf(black_fp, "%s\n", item_str);	// write to log file
 		}
-		shmptr->finished_producers++;
-		printf("%s\n", "black producer finished");
 
 		if (fclose(black_fp) != 0)
 		{
@@ -112,8 +108,6 @@ int main(int argc, char* argv[])
 			pthread_cond_signal(&(shmptr->item_available));
 			fprintf(white_fp, "%s\n", item_str);
 		}
-		shmptr->finished_producers++;
-		printf("%s\n", "white producer finished");
 		
 		if (fclose(white_fp) != 0)
 		{
@@ -148,8 +142,6 @@ int main(int argc, char* argv[])
 			pthread_cond_signal(&(shmptr->item_available));
 			fprintf(red_fp, "%s\n", item_str);
 		}
-		shmptr->finished_producers++;
-		printf("%s\n", "red producer finished");
 		
 		if (fclose(red_fp) != 0)
 		{
@@ -165,7 +157,9 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 
+	// detach from shared memory segment & return
 	shmdt((void *) shmptr);
+
 	return 0;
 }
 
